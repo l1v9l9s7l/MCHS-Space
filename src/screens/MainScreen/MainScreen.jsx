002b930e4f542bd13cca3760ab701b96a86c3527
l1v9l9s7l <l1v9l9s7l@gmail.com>
2024-12-10 20:23:10 +0300

    init commit

---
+++ src/screens/MainScreen/MainScreen.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default function MainScreen() {
+
+  return(
+    <View>
+      <Text>MCHS-Space</Text>
+    </View>
+  )
+}
